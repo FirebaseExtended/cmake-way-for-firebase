81ddc002075ae589607dcf01023eff73ad63406f
Patrick Martin <martinpatrick@google.com>
2020-09-11 11:15:52 -0600

    deleted the hello_firebase project, and moved popsicle_platformer to the root

--- popsicle_runner/third_party/download.ps1
+++
@@ -1,3 +0,0 @@
-Invoke-WebRequest "https://dl.google.com/firebase/sdk/cpp/firebase_cpp_sdk_6.15.1.zip" -OutFile "$PSScriptRoot/firebase_cpp_sdk.zip"
-Expand-Archive "$PSScriptRoot/firebase_cpp_sdk.zip"
-Remove-Item -Path "$PSScriptRoot/firebase_cpp_sdk.zip"
