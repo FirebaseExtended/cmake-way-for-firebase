59678c2d14e5946a69f11a843b409201c1da1ebe
Patrick Martin <martinpatrick@google.com>
2020-09-15 11:40:58 -0600

    Improved the clean build speed by about 10 minutes

--- third_party/cocos2d/cocos/base/ccConfig.h
+++ third_party/cocos2d/cocos/base/ccConfig.h
@@ -270,7 +270,7 @@ THE SOFTWARE.
 /** Use 3d physics integration API. */
 #ifndef CC_USE_3D_PHYSICS
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS || CC_TARGET_PLATFORM == CC_PLATFORM_MAC || CC_TARGET_PLATFORM == CC_PLATFORM_WIN32 || CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID || CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
-#define CC_USE_3D_PHYSICS 1
+#define CC_USE_3D_PHYSICS 0
 #endif
 #endif
 
@@ -283,7 +283,7 @@ THE SOFTWARE.
 
 /** Use 3D navigation API */
 #ifndef CC_USE_NAVMESH
-#define CC_USE_NAVMESH 1
+#define CC_USE_NAVMESH 0
 #endif
 
 /** Use culling or not. */
@@ -300,13 +300,13 @@ THE SOFTWARE.
 /** Support JPEG or not. If your application don't use jpeg format picture, you can undefine this macro to save package size.
  */
 #ifndef CC_USE_JPEG
-#define CC_USE_JPEG  1
+#define CC_USE_JPEG  0
 #endif // CC_USE_JPEG
 
 /** Support webp or not. If your application don't use webp format picture, you can undefine this macro to save package size.
  */
 #ifndef CC_USE_WEBP
-#define CC_USE_WEBP  1
+#define CC_USE_WEBP  0
 #endif // CC_USE_WEBP
 
 /** Enable Script binding. */
